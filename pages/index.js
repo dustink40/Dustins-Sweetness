49f75b93896988da8c1e79964d94a5c919835f90
Dustykeith83 <133188662+dustink40@users.noreply.github.com>
2023-08-30 10:12:01 +0000

    fixed

--- pages/index.js
+++ pages/index.js
@@ -139,8 +139,9 @@ const Home = () => {
           </div>
           <div classname="logo-container">
           <Image style={imageStyle} src={codieLogo} alt="Pretty girl" />
-          <Image style={imageStyle} src={codie2Logo} alt="Pretty girl" />
           <Image style={imageStyle} src={trueloveLogo} alt="Pretty girl" />
+          <Image style={imageStyle} src={codie2Logo} alt="Pretty girl" />
+          
           </div>
           <div className="header-subtitle">
             <h2>Digging in Codie's guts like a gardner, If she starts screaming i'ma dig a little harder</h2>
